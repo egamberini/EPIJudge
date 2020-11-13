a1a4bd58cc2d5c2563a81dff6eccb8952f14e6f0
Enrico Gamberini <enrico.gamberini@cern.ch>
2020-11-13 17:35:05 +0100

    solve closest_int_same_weight

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,33 @@
 #include "test_framework/generic_test.h"
+
+unsigned ComputeWeight(unsigned long long x) {
+  unsigned w = 0;
+  while (x != 0) {
+    x &= (x - 1);
+    w += 1;
+  }
+  return w;
+}
+
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  unsigned w = ComputeWeight(x);
+  unsigned long long lowest_set_bit = x & ~(x-1);
+  if (lowest_set_bit != 1) {
+    x -= lowest_set_bit;
+    lowest_set_bit >>= 1;
+    x += lowest_set_bit;
+  } else {
+    unsigned long long x1 = x;
+    unsigned pos = 0;
+    while ((x1 & 1) == 1) {
+      x1 >>= 1;
+      pos += 1;
+    }
+    x -= (1UL << (pos-1));
+    x += (1UL << (pos));
+  }
+  return x;
+
 }
 
 int main(int argc, char* argv[]) {
